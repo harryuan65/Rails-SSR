d60f5ee581e8283acc7ce5ca1331d68de2b11522
harryuan65 <harryuan.65@gmail.com>
2022-02-19 08:40:11 +0800

    chore(ruby): bump version to 3.1.1 stable, add configs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
+# Root
 class ApplicationController < ActionController::Base
   def index; end
+
+  def show
+    event = Event.find(params[:id])
+    render inertia: 'Event/Show',
+           props: {
+             event: event.as_json(
+               only: %i[id title start_date description]
+             )
+           }
+  end
 end
